dd16203dc1b231c68f0794b548ebf4413a91bd92
DJ <social.deej@gmail.com>
2022-02-09 02:09:13 +0530

    fixes: js integration

---
+++ MWA-Lab-Assignment-2/Question 2/index.js
@@ -0,0 +1,23 @@
+console.log("hello");
+
+function myFunction() {
+    let phoneNumber = document.getElementById("Phone").value;
+    let emailId = document.getElementById("E-mail-ID").value;
+
+    let password = document.getElementById("Password").value;
+    let retypePassword = document.getElementById("Retype-Password").value;
+
+    if ( password != retypePassword )
+        alert("Please Match your password")
+
+    if ( phoneNumber.length != 10 || isNaN(phoneNumber))
+        alert("Phone Number Incorrect");
+    
+        
+    var searchIndex = emailId.indexOf("@");
+    var searchChangeCom = emailId.lastIndexOf(".");
+    if (searchIndex < 1 || searchChangeCom < searchIndex + 2 || searchChangeCom + 2 >= emailId.length )
+       alert("Please Enter a valid Email Address");
+
+    
+}
